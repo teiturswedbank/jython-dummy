ce8ceaec972fa663bede36799fb460d038210751
swedbank <teitur.arnarson@swedbank.se>
2019-09-25 09:47:24 +0200

    Simpler test app

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+#from flask import Flask
+#app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+#@app.route('/')
+#def hello_world():
+#    return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+#    app.run(host='0.0.0.0', port=8080)
+    from time import sleep
+    i=0
+    while True:
+        i+=1
+        print("Iter: %d" % i)
+        sleep(2)
